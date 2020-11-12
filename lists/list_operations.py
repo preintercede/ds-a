f2c01a12f8524914ecf536a58c37bd2703843ac0
Dylan <dylanw@uci.edu>
2020-11-12 09:57:07 -0800

    some list operations

---
+++ lists/list_operations.py
@@ -0,0 +1,35 @@
+shoppingList = ['Milk', 'Cheese', 'Butter']
+
+
+# for i in range(len(shoppingList)):
+#     shoppingList[i] = shoppingList[i] + "+"
+#     print(shoppingList[i])
+
+
+myList = [1, 2, 3, 4, 5, 6, 7]
+# print(myList)
+# myList.insert(0, 11) # beginning of list
+# print(myList)
+# myList.append(55) # end of list
+# print(myList)
+
+# newList = [11, 12, 13, 14]
+# myList.extend(newList)
+# print(myList)
+
+varList = ['a', 'b', 'c', 'd', 'e', 'f']
+
+print(varList[1:])
+
+varList[0:2] = ['x', 'y']
+
+print(varList)
+
+# varList.pop(1)
+# print(varList)
+
+# del varList[1]
+# print(varList)
+
+varList.remove('e')
+print(varList)
