9f188de18d93a870373799a26c368ac280913ab3
Dylan <dylanw@uci.edu>
2020-11-17 11:31:41 -0800

    traverse through a dictonary

--- dictionaries/dictionary.py
+++ dictionaries/dictionary.py
@@ -1,4 +1,14 @@
 myDict = {'name': 'Bob', "age": 20}
-myDict['age'] = 21
-myDict['address'] = '123 Street St'
-print(myDict)
+myDict['age'] = 21  # O(1)
+myDict['address'] = '123 Street St'  # O(1)
+# print(myDict)
+
+newDict = {'name': 'Bobby', 'age': 30, 'city': 'Los Angeles'}
+
+
+def traverseDict(dict):
+    for key in dict:  # O(n)
+        print(key, dict[key])
+
+
+# traverseDict(newDict)
