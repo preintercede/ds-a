d0940f9c17f66857f6e01e047bc6b26ba83f8dee
Dylan <dylanw@uci.edu>
2020-11-17 17:52:46 -0800

    traverse a tuple

--- tuples/tuples.py
+++ tuples/tuples.py
@@ -1,5 +1,13 @@
 newTuple = 'a', 'b', 'c', 'd', 'e'
-newTuple1 = tuple('abcde')
+# newTuple1 = tuple('abcde')
 
-print(newTuple)
-print(newTuple1)
+# print(newTuple)
+# print(newTuple1)
+
+# print(newTuple[1])
+
+for i in newTuple:
+    print(i)
+
+for i in range(len(newTuple)):  # O(n)
+    print(newTuple[i])
