ee98e7683b6840b14686e88d932b6ce10f364ecc
Dylan <dylanw@uci.edu>
2020-11-17 17:57:08 -0800

    search tuple

--- tuples/tuples.py
+++ tuples/tuples.py
@@ -6,8 +6,18 @@ newTuple = 'a', 'b', 'c', 'd', 'e'
 
 # print(newTuple[1])
 
-for i in newTuple:
-    print(i)
+# for i in newTuple:
+#     print(i)
 
-for i in range(len(newTuple)):  # O(n)
-    print(newTuple[i])
+# for i in range(len(newTuple)):  # O(n)
+#     print(newTuple[i])
+
+
+def searchTuple(pTuple, value):
+    for i in pTuple:  # O(n)
+        if i == value:
+            return pTuple.index(i)
+    return 'Value not found in the tuple'
+
+
+print(searchTuple(newTuple, 'b'))
